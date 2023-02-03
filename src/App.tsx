4a3ff41332d571bc4550526266d5abca02572ca3
MingXue <ming@twotone.co>
2023-02-03 09:46:40 +0800

    update app tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,46 @@
-import { SignUpForm } from "./components/index";
+import React from 'react'
+import { SignUpForm } from './components/index'
 
 interface IFormProps {
-  firstName?: string;
-  lastName?: string;
-  address?: string | undefined;
-  zipCode?: string;
-  city?: string;
-  phoneNumber?: string;
-  email?: string;
-  cardNumber?: string;
+  firstName?: string
+  lastName?: string
+  address?: string | undefined
+  zipCode?: string
+  city?: string
+  phoneNumber?: string
+  email?: string
+  cardNumber?: string
 }
 
 function App() {
   const onSubmit = (data: IFormProps) => {
-    console.log(data);
-  };
+    console.log(data)
+  }
 
   return (
     <div
       style={{
-        display: "flex",
-        width: "100%",
-        height: "100%",
-        justifyContent: "center",
-        alignItems: "center",
-        paddingTop: "149px",
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: '149px',
       }}
     >
       <SignUpForm
-        firstColor="#88B431"
-        secondColor="#D9D9D9"
-        headingTitle={["Enter Your Information", "Enter Your Payment Details"]}
+        firstColor='#88B431'
+        secondColor='#D9D9D9'
+        headingTitle={['Enter Your Information', 'Enter Your Payment Details']}
         subCaption={[
-          "Please fill out the following fields to create an account: *Email and password are case sensitive",
-          "",
+          'Please fill out the following fields to create an account: *Email and password are case sensitive',
+          '',
         ]}
         steps={2}
         onSubmit={onSubmit}
       />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
