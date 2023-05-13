d60c7cf16785c38e44b9846eed9bed1657e60f06
ming <ming@twotone.co>
2023-05-13 08:29:14 +0800

    dfd

--- src/components/SignUpForm/StepButtons.tsx
+++ src/components/SignUpForm/StepButtons.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ForwardIcon from '@mui/icons-material/Forward'
 import { IStepButtons } from './types'
-import { Badge, ButtonGroup, ButtonIconWrapper, Loader, StepButton, Text } from './styles'
+import { BackButton, Badge, ButtonGroup, ButtonIconWrapper, Loader, StepButton, Text } from './styles'
 import { BadgeIcon, LockIcon, SubmitLoading } from './Icons'
 
-export const StepButtons = ({ currentStep, loading }: IStepButtons) => {
+export const StepButtons = ({ currentStep, loading, back }: IStepButtons) => {
   return (
     <ButtonGroup marginTop={currentStep === 2} className='sf-sign-up-form-buttons'>
-      {/* {currentStep === 2 && <BackButton onClick={back}>Back</BackButton>} */}
+      {currentStep === 2 && <BackButton onClick={back}>Back</BackButton>}
       <StepButton
         type='submit'
         disabled={loading}
